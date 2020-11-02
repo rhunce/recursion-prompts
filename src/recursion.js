101e30af6938c7da462d06f9418a65d126aca82b
Raymond Hunce <hunce18c@gmail.com>
2020-11-02 18:54:17 -0500

    Implemented and passed tests for range function

--- src/recursion.js
+++ src/recursion.js
@@ -102,7 +102,27 @@ var sumBelow = function(n) {
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
+
 var range = function(x, y) {
+
+  if (Math.abs(x - y) <= 1) {
+    return [];
+  }
+
+  if (x < y) {
+    if (y - x === 2) {
+      return x + 1;
+    }
+    return [x + 1, range(x + 1, y)].flat();
+  }
+
+  if (x > y) {
+    if (x - y === 2) {
+      return x - 1;
+    }
+    return [x - 1, range(x - 1, y)].flat();
+  }
+
 };
 
 // 7. Compute the exponent of a number.
