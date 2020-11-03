f2078d89ef7a7605088effc3752fe3dc87964946
Raymond Hunce <hunce18c@gmail.com>
2020-11-02 20:11:36 -0500

    Implemented and passed tests for reverse function

--- src/recursion.js
+++ src/recursion.js
@@ -171,6 +171,11 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  if (string.length === 1) {
+    return string;
+  }
+  var lastIndex = string.length - 1;
+  return string[lastIndex] + reverse(string.substring(0, lastIndex));
 };
 
 // 10. Write a function that determines if a string is a palindrome.
