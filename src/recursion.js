e8cc22ee7480005380f3e1b4942d49269aa0072e
Raymond Hunce <hunce18c@gmail.com>
2020-11-09 13:15:59 -0500

    Implemented and passed tests for nestedEvenSum function

--- src/recursion.js
+++ src/recursion.js
@@ -569,7 +569,18 @@ var capitalizeFirstLetter = function(word) {
 //   e: {e: {e: 2}, ee: 'car'}
 // };
 // nestedEvenSum(obj1); // 10
+
 var nestedEvenSum = function(obj) {
+  var sum = 0;
+  for (var key in obj) {
+    if (obj[key] % 2 === 0) {
+      sum += obj[key];
+    }
+    if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+      sum += nestedEvenSum(obj[key]);
+    }
+  }
+  return sum;
 };
 
 // 30. Flatten an array containing nested arrays.
